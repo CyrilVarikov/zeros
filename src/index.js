50a00e663845f2bd25965c5eb003ab2e48a6eb11
Kirill Varikov <35407446+CyrilVarikov@users.noreply.github.com>
2018-02-23 15:30:37 +0300

    Implement seventh task

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
 module.exports = function getZerosCount(number) {
   // 1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10 * 11 * 12 * 13 * 14 * 15 * 16 * 17 * 18 * 19 * 20
-  var two = 0, five = 0;
-  for (var i = 1; i <= number; i++) {
-    factor = i;
-    while (((factor % 2) === 0) || ((factor % 5) === 0)) {
-      if (factor % 2 === 0) {
-        two++;
-        factor /= 2;
-      }
-      if (factor % 5 === 0) {
-        five++;
-        factor /= 5;
-      }
-    }
+  var countZero = 0;
+  while (number >= 5) {
+    number = Math.floor(number / 5);
+    countZero += number;
   }
-   if (five < two) {
-     return five;
-   } else {
-     return two;
-   }
+return countZero;
+
 };
--- src/temp.js
+++ src/temp.js
@@ -1,20 +1,8 @@
-number = 20;
-var two = 0, five = 0;
-for (var i = 1; i <= number; i++) {
-  factor = i;
-  while (((factor % 2) === 0) || ((factor % 5) === 0)) {
-    if (factor % 2 === 0) {
-      two++;
-      factor /= 2;
-    }
-    if (factor % 5 === 0) {
-      five++;
-      factor /= 5;
-    }
-  }
+
+number = 100;
+var countZero = 0;
+while (number >= 5) {
+  number = Math.floor(number / 5);
+  countZero += number;
 }
- if (five < two) {
-   console.log(five);
- } else {
-   console.log(two);
- }
+console.log(countZero);
